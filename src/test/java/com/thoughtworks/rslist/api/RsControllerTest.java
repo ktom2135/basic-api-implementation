788ca68c1ef9be1c83a14d538b2d7d6b349ff2ff
Chen Xu <xuchen@thoughtworks.com>
2020-08-03 23:14:50 +0800

    implement get rs event between

--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -26,6 +26,19 @@ class RsControllerTest {
     }
 
     @Test
+    void shouldGetRsEventBetween() throws Exception {
+        mockMvc.perform(get("/rs/list?start=1&end=2"))
+                .andExpect(content().string("[第一条事件, 第二条事件]"))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list?start=2&end=3"))
+                .andExpect(content().string("[第二条事件, 第三条事件]"))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list?start=1&end=3"))
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
     void shouldGetOneRsEvent() throws Exception {
         mockMvc.perform(get("/rs/1"))
                 .andExpect(content().string("第一条事件"))
