791429f1961eb870fec830ddb74c863c676ed4c5
ChenXu <xuchen@thoughtworks.com>
2020-09-14 21:47:15 +0800

    [chenxu]: implement get rs list

---
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.rslist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void should_get_rs_list() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/rs/list"))
+                .andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        assertEquals(200, response.getStatus());
+        assertEquals("[第一条事件, 第二条事件, 第三条事件]", response.getContentAsString());
+    }
+
+}
